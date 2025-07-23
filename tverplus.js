0f067574dd63ee30309792973a5b239cc34eb04d
e0406370 <e0406370@u.nus.edu>
2025-07-24 01:46:29 +0800

    [feat] retrieve series title directly from html
    GH-4

--- tverplus.js
+++ tverplus.js
@@ -1,15 +1,64 @@
 // ==UserScript==
 // @name         tverplus
 // @namespace    tver
-// @description  Adds MyDramaList rating and link to the corresponding MyDramaList page directly on TVer series pages. 1-1 matching is not guaranteed. 
+// @description  Adds MyDramaList rating and link to the corresponding MyDramaList page directly on TVer series pages. 1-1 matching is not guaranteed.
 // @author       e0406370
 // @include      https://tver.jp/series/*
 // @version      2025-07-23
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+class Constants {
+  static get seriesTitleClass() {
+    return "series-main_title";
+  }
 
-    alert('test')
-})();
+  static get seriesContentClass() {
+    return "series-main_content";
+  }
+}
+Object.freeze(Constants);
+
+class Utils {
+  static retrieveSelectorClassStartsWith(className) {
+    return `[class^=${className}]`;
+  }
+}
+Object.freeze(Utils);
+
+
+function waitForTitle() {
+  let titleSelector = Utils.retrieveSelectorClassStartsWith(Constants.seriesTitleClass);
+  let fetchTitleElement = function () { return document.querySelector(titleSelector); };
+
+  return new Promise((res) => {
+    let titleElement = fetchTitleElement();
+    if (titleElement) {
+      return res(titleElement.textContent);
+    }
+
+    const observer = new MutationObserver(() => {
+      let titleElement = fetchTitleElement();
+      if (titleElement) {
+        observer.disconnect();
+        res(titleElement.textContent);
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+function init() {
+  let title;
+
+  waitForTitle().then((res) => {
+    console.info(res);
+    title = res;
+  });
+}
+
+init();
