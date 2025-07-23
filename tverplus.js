216ea4cc81696d903720a2d1c8ee540b811ff069
e0406370 <e0406370@u.nus.edu>
2025-07-23 22:41:06 +0800

    [feat] setup working userscript template
    GH-2

---
+++ tverplus.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         tverplus
+// @namespace    tver
+// @description  Adds MyDramaList rating and link to the corresponding MyDramaList page directly on TVer series pages. 1-1 matching is not guaranteed. 
+// @author       e0406370
+// @include      https://tver.jp/series/*
+// @version      2025-07-23
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert('test')
+})();
